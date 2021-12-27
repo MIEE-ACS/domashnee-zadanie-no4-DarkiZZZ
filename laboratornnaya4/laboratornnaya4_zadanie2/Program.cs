573a5cd6fdd7b1c1186d2968bcc075f7fa4f0056
Mike <darkizfollower@gmail.com>
2021-12-27 21:35:38 +0300

    .

---
+++ laboratornnaya4/laboratornnaya4_zadanie2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace laboratornnaya4_zadanie2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
